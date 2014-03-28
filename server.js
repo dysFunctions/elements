18297b5e3346d82a57592ac75769201a1d95abc3
James Stiehl <jstiehl@James-Stiehls-MacBook-Pro.local>
2014-03-28 13:26:33 -0700

    removing my API key from server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var mime = require('mime');
 var path = require('path');
-var db = require('orchestrate')('24512bf3-f66d-40fd-bc7e-144753e2f84e')
+var db = require('orchestrate')('Inser Key Here');
 
 http.createServer(function(req, res){
 	var pathname = url.parse(req.url, true).pathname;
