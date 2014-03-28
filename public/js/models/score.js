1f19d759101f32fe45a504ecc088e0fc64be98da
James Stiehl <jstiehl@gmail.com>
2014-03-27 18:48:34 -0700

    added some api routes to handle GET and POST requests

---
+++ public/js/models/score.js
@@ -0,0 +1,9 @@
+define(function(require){
+
+  var Backbone = require('backbone');
+
+  var Score = Backbone.Model.extend({});
+
+  return Score;
+
+});
---
+++ public/js/views/scoresView.js
@@ -0,0 +1,27 @@
+define(function(require){
+
+    var Backbone = require('backbone');
+
+    var ScoresView = Backbone.View.extend({
+      el: "#scores-table",
+
+      initialize: function(){
+        this.render();
+      },
+
+      render: function(){
+        var body = '';
+
+        this.collection.each(function(model){
+          body += '<tr><td>'+model.attributes.name+'</td><td>'+model.attributes.score+'</td></tr>';
+          return body;
+        });
+
+        this.$el.find("tbody").append(body);
+      }
+    });
+
+
+  return ScoresView;
+
+});
