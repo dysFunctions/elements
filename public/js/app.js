522c195e0d3fbb59edec416bd3a9777d31ad7e0f
abbystevens <adeats@gmail.com>
2014-04-23 08:34:56 -0700

    messed with index.html and main.css, added router

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,7 @@ define(function (require){
     var Scores = require('collections/scores')
     var ScoresView = require('views/scoresView');
     var TableView = require('views/tableView');
+    var Router = require('router');
     var $ = require('jquery');
 
     $(function(){
@@ -50,6 +51,15 @@ define(function (require){
           });
 
         });
+
+      var router = new Router({
+        quiz: elementView,
+        highScores: scoresView,
+        periodicTable: tableView,
+        about: about
+      });
+
+      Backbone.history.start();
 
 
 
